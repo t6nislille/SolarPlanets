df01981d88fc65e180bb27d54c73eca7f576acd2
Tõnis <tonislille@gmail.com>
2023-04-26 17:02:24 +0300

    Planets details page

---
+++ SolarPlanets/Views/PlanetDetailsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SolarPlanets.Views;
+
+    public partial class PlanetDetailsPage : ContentPage
+    {
+        public PlanetDetailsPage(Planet planet)
+        {
+            InitializeComponent();
+            this.BindingContext = planet;
+        }
+
+        async void BackButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
